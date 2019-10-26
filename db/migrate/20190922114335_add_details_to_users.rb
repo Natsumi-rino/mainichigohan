f2641483beda6752960c09697170dd3c256e53ae
Natsumi Sugimachi <himegoto12@gmail.com>
2019-10-26 18:40:52 +0900

    released to Heroku

--- db/migrate/20190922114335_add_details_to_users.rb
+++ db/migrate/20190922114335_add_details_to_users.rb
@@ -1,7 +1,7 @@
 class AddDetailsToUsers < ActiveRecord::Migration[5.1]
   def change
     add_column :users, :username, :string
-    add_column :users, :profile, :text
+    # add_column :users, :profile, :text
     add_column :users, :place, :integer
     add_column :users, :age, :integer
     add_column :users, :sex, :integer
--- db/schema.rb
+++ db/schema.rb
@@ -60,6 +60,8 @@ ActiveRecord::Schema.define(version: 20191026044233) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.text "profile"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -67,8 +69,6 @@ ActiveRecord::Schema.define(version: 20191026044233) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
-    t.text "profile"
     t.integer "place"
     t.integer "age"
     t.integer "sex"
